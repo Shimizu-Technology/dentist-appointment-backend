7ab5bf5c2b4e0eae6636cbaa64e80d1b8897fbf8
Leon Shimizu <lmshimizu@gmail.com>
2024-12-31 21:28:32 +1000

    Add admin? helper method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Devise modules: confirmable, lockable, timeoutable, trackable, etc. if needed
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def admin?
+    role == "admin"
+  end
 end
