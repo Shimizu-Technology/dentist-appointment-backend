55a667d1b8a68b05367d4278c591f7fed86b4a9d
Leon Shimizu <lmshimizu@gmail.com>
2025-01-01 01:38:24 +1000

    Finalize appointment booking logic with time conflict checks

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,10 +1,14 @@
 class Appointment < ApplicationRecord
   belongs_to :user
-  belongs_to :dependent
-  belongs_to :dentist
-  belongs_to :appointment_type
-  belongs_to :user
   belongs_to :dependent, optional: true
   belongs_to :dentist
   belongs_to :appointment_type
+
+  before_create :set_default_status
+
+  private
+
+  def set_default_status
+    self.status ||= 'scheduled'
+  end
 end
