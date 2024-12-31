4a2634c992cc007161eaaaf6303b89dc3f1b5679
Leon Shimizu <lmshimizu@gmail.com>
2025-01-01 01:04:38 +1000

    Remove Devise macros and add has_secure_password to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
-  # Devise modules: confirmable, lockable, timeoutable, trackable, etc. if needed
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
+  has_secure_password
+  
   has_many :dependents, dependent: :destroy
   has_many :appointments
 
