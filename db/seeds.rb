17459966a18f64373a3a23cf4a83421c0109d27d
Leon Shimizu <lmshimizu@gmail.com>
2025-01-01 17:34:19 +1000

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@ Appointment.destroy_all
 AppointmentType.destroy_all
 DentistAvailability.destroy_all
 Dentist.destroy_all
+Specialty.destroy_all
 Dependent.destroy_all
 User.destroy_all
 
@@ -17,8 +18,8 @@ admin_user = User.create!(
   policy_number: "AAA111",
   plan_type: "PPO",
   phone: "555-0001",
-  first_name: "Admin",
-  last_name: "User"
+  first_name: "Adminy",
+  last_name: "McAdmin"
 )
 
 parent_user = User.create!(
@@ -29,8 +30,8 @@ parent_user = User.create!(
   policy_number: "BBB222",
   plan_type: "HMO",
   phone: "555-0002",
-  first_name: "John",
-  last_name: "Parent"
+  first_name: "Parent",
+  last_name: "User"
 )
 
 puts "Creating Dependents..."
@@ -46,33 +47,45 @@ parent_dependent = parent_user.dependents.create!(
   date_of_birth: "1980-01-01"
 )
 
+puts "Creating Specialties..."
+general_specialty = Specialty.create!(name: "General Dentistry")
+pediatric_specialty = Specialty.create!(name: "Pediatric Dentistry")
+adult_specialty = Specialty.create!(name: "Adult Dentistry")
+
 puts "Creating Dentists..."
 dentist_adult = Dentist.create!(
   first_name: "Jane",
   last_name: "Doe",
-  specialty: "Adult Dentistry"
+  specialty: adult_specialty,
+  image_url: "https://images.unsplash.com/photo-1234...",
+  qualifications: "DDS from ABC University\n15+ years experience"
 )
 
 dentist_pediatric = Dentist.create!(
   first_name: "Joe",
   last_name: "Kiddo",
-  specialty: "Pediatric Dentistry"
+  specialty: pediatric_specialty,
+  image_url: "https://images.unsplash.com/photo-5678...",
+  qualifications: "DMD from XYZ University\nCertified Pediatric Dentist"
 )
 
 puts "Creating Appointment Types..."
 cleaning_type = AppointmentType.create!(
   name: "Cleaning",
-  description: "Routine cleaning"
+  description: "Routine cleaning",
+  duration: 30
 )
 
 filling_type = AppointmentType.create!(
   name: "Filling",
-  description: "Cavity filling"
+  description: "Cavity filling",
+  duration: 45
 )
 
 checkup_type = AppointmentType.create!(
   name: "Checkup",
-  description: "General checkup"
+  description: "General checkup",
+  duration: 20
 )
 
 puts "Creating Appointments..."
@@ -95,22 +108,23 @@ Appointment.create!(
 )
 
 puts "Creating Dentist Availabilities..."
-DentistAvailability.create!(dentist: dentist_adult,    day_of_week: 1, start_time: "09:00", end_time: "17:00")
-DentistAvailability.create!(dentist: dentist_adult,    day_of_week: 2, start_time: "09:00", end_time: "17:00")
-DentistAvailability.create!(dentist: dentist_adult,    day_of_week: 3, start_time: "09:00", end_time: "17:00")
-DentistAvailability.create!(dentist: dentist_adult,    day_of_week: 4, start_time: "09:00", end_time: "17:00")
-DentistAvailability.create!(dentist: dentist_adult,    day_of_week: 5, start_time: "09:00", end_time: "15:00")
+DentistAvailability.create!(dentist: dentist_adult, day_of_week: 1, start_time: "09:00", end_time: "17:00") # Monday
+DentistAvailability.create!(dentist: dentist_adult, day_of_week: 2, start_time: "09:00", end_time: "17:00") # Tuesday
+DentistAvailability.create!(dentist: dentist_adult, day_of_week: 3, start_time: "09:00", end_time: "17:00") # Wed
+DentistAvailability.create!(dentist: dentist_adult, day_of_week: 4, start_time: "09:00", end_time: "17:00") # Thur
+DentistAvailability.create!(dentist: dentist_adult, day_of_week: 5, start_time: "09:00", end_time: "15:00") # Fri
 
-DentistAvailability.create!(dentist: dentist_pediatric, day_of_week: 1, start_time: "10:00", end_time: "18:00")
-DentistAvailability.create!(dentist: dentist_pediatric, day_of_week: 2, start_time: "10:00", end_time: "18:00")
-DentistAvailability.create!(dentist: dentist_pediatric, day_of_week: 4, start_time: "10:00", end_time: "18:00")
-DentistAvailability.create!(dentist: dentist_pediatric, day_of_week: 5, start_time: "10:00", end_time: "16:00")
+DentistAvailability.create!(dentist: dentist_pediatric, day_of_week: 1, start_time: "10:00", end_time: "18:00") # Monday
+DentistAvailability.create!(dentist: dentist_pediatric, day_of_week: 2, start_time: "10:00", end_time: "18:00") # Tuesday
+DentistAvailability.create!(dentist: dentist_pediatric, day_of_week: 4, start_time: "10:00", end_time: "18:00") # Thurs
+DentistAvailability.create!(dentist: dentist_pediatric, day_of_week: 5, start_time: "10:00", end_time: "16:00") # Fri
 
 puts "Seeding complete!"
 puts "Created:"
 puts " - 1 Admin User (admin@example.com / password)"
 puts " - 1 Regular User (parent@example.com / password) with 2 dependents"
-puts " - 2 Dentists (Adult / Pediatric)"
-puts " - 3 Appointment Types (Cleaning, Filling, Checkup)"
+puts " - 3 Specialties (General, Pediatric, Adult)"
+puts " - 2 Dentists referencing those specialties"
+puts " - 3 Appointment Types (Cleaning, Filling, Checkup) with durations"
 puts " - 2 Appointments"
-puts " - DentistAvailability (Schedules) for each dentist"
+puts " - DentistAvailability for each dentist"
