3974b24d4afbed147c82c30f573d37685a15cf58
Leon Shimizu <lmshimizu@gmail.com>
2025-01-01 15:29:44 +1000

    Add cors and fix configuration

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem "pg", "~> 1.1"
 
 gem 'bcrypt', '~> 3.1.7'
 gem 'jwt', '~> 2.2'
+gem 'rack-cors'
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", ">= 5.0"
--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -2,9 +2,6 @@
 module Api
   module V1
     class BaseController < ApplicationController
-      protect_from_forgery with: :null_session
-      respond_to :json
-
       before_action :authenticate_user!
 
       # Custom JWT-based authentication for API calls
