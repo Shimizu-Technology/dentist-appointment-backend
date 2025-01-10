0ca31db94aa55e2044279f7017be2b9dc4131a07
Leon Shimizu <lmshimizu@gmail.com>
2025-01-10 16:22:22 +1000

    Hardcode the url to the prod site for now to test

--- app/jobs/send_appointment_reminders_job.rb
+++ app/jobs/send_appointment_reminders_job.rb
@@ -4,7 +4,6 @@ class SendAppointmentRemindersJob < ApplicationJob
   queue_as :default
 
   def perform
-    # 1) Fetch all reminders that are unsent and due
     Rails.logger.debug("[SendAppointmentRemindersJob] Looking for unsent reminders due <= #{Time.current}")
     due_reminders = AppointmentReminder.unsent
                                        .where("send_at <= ?", Time.current)
@@ -12,11 +11,9 @@ class SendAppointmentRemindersJob < ApplicationJob
     Rails.logger.debug("[SendAppointmentRemindersJob] Found #{due_reminders.size} reminders.")
 
     due_reminders.find_each do |reminder|
-      # 2) Send
       Rails.logger.debug("[SendAppointmentRemindersJob] --> Sending reminder ID=#{reminder.id}, AppointmentID=#{reminder.appointment_id}.")
       send_reminder(reminder)
 
-      # 3) Mark as sent
       Rails.logger.debug("[SendAppointmentRemindersJob] --> Marking reminder ID=#{reminder.id} as sent.")
       reminder.mark_sent!
     end
@@ -28,7 +25,7 @@ class SendAppointmentRemindersJob < ApplicationJob
 
   def send_reminder(reminder)
     appointment = reminder.appointment
-    user = appointment.user
+    user        = appointment.user
 
     # If user has no phone number, skip
     if user.phone.blank?
@@ -39,12 +36,12 @@ class SendAppointmentRemindersJob < ApplicationJob
     # Create a user-friendly time string
     appt_time_str = appointment.appointment_time.strftime('%B %d at %I:%M %p')
 
-    # Build the SMS body
+    # Build the SMS body with a hardcoded URL
     message_body = <<~TEXT.squish
       Hi #{user.first_name}, 
       this is a reminder from ISA Dental for your appointment on #{appt_time_str}.
       If you need to reschedule or cancel, please call us at (671) 646-7982
-      or visit #{Rails.application.config.x.frontend_url} for more options.
+      or visit https://isa-dental-appt.netlify.app for more options.
     TEXT
 
     Rails.logger.debug("[SendAppointmentRemindersJob] Attempting to send SMS to #{user.phone} (ReminderID=#{reminder.id}).")
